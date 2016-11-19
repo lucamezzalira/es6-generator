b2a82fe74a1671a87ac92505584e89467d802e78
Luca Mezzalira <mezzalab@gmail.com>
2016-11-19 05:47:51 +0000

    added template and dependencies installation

---
+++ app/index.js
@@ -0,0 +1,41 @@
+var generators = require('yeoman-generator');
+
+module.exports = generators.Base.extend({
+
+  constructor: function () {
+    generators.Base.apply(this, arguments);
+    this.argument('appname', { type: String, required: true });
+    this.installDependencies({
+        npm: true,
+        callback: function () {
+            console.log('Project ready!');
+        }
+    });
+  },
+  writing: {
+      config: function(){
+          this.fs.copyTpl(
+              this.templatePath('package.json'),
+              this.destinationPath('package.json'),
+              {
+                  name: this.appname
+              }
+          );
+          this.fs.copyTpl(
+              this.templatePath('index.html'),
+              this.destinationPath('index.html'),
+              {
+                  name: this.appname
+              }
+          );
+          this.fs.copy(
+              this.templatePath('src/App.js'),
+              this.destinationPath('src/App.js')
+          )
+      }
+  },
+  end: {
+
+  }
+
+});
